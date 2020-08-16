de597a2708ab3ec70c685652b7d13d9304428656
gargnandini <nandinigarh1204@gmail.com>
2020-08-16 17:47:12 +0530

    everything is working fine!

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 // Global app controller//
+
+import str from './models/Search';
+//import { add, multiply, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 2)}, ${str}`);
